8a43cd132173832d761607689dd4ec1fa7a35e8e
MZ-21 <mijgonzekria@gmail.com>
2023-09-03 18:45:43 -0400

    functions added

--- SwiftNotes/scripts/text-features.js
+++ SwiftNotes/scripts/text-features.js
@@ -13,44 +13,66 @@ textContainer.addEventListener("click",(e)=>{
     toggleTextOptions();
 });
 
-//CHANGING TEXT WHEN BUTTON IS SELECTED
-
-const timesNewRomanBtn = document.getElementById("Times-New-Roman-btn");
-const arialBtn = document.getElementById("Arial-btn");
-const pagesSelected = document.getElementById("base-page-1"); //could later change to a specific page
-
-
-timesNewRomanBtn.addEventListener("click",function(){
-    console.log("red");
-    pagesSelected.style.color = "red";
-    // const selectedText = window.getSelection().toString();
-    
-    // if(selectedText.length >0){
-    //     //wrapping selected text in span
-    //    const span = document.createElement("span");
-    //    span.className = "add-Times-New-Roman";
-    //    span.textContent = selectedText;
-        
-    //    const text = pagesSelected.textContent;
-    //    const replace = text.replace(selectedText,span.outerHTML);
-
-    //    pagesSelected.innerHTML = replace; //replacing old with new
-
-    // }
-
-
+document.documentElement.addEventListener("click", ()=>{
+    //checking if has been toggled
+    if(textDropDown.classList.contains("show-drop-down-menu-text-icon")){
+        toggleTextOptions();
+    }
 });
 
-// const toggleFontFamily = function(classL){
-//     pagesSelected.toggle.classList(classL);
-// }
-// timesNewRomanBtn.addEventListener("click",(e)=>{
-//     e.stopPropagation();
-//     toggleFontFamily("add-Times-New-Roman");
-// });
-// arialBtn.addEventListener("click",(e)=>{
-//     console.log("A");
-//     e.stopPropagation();
-//     toggleFontFamily("add-Arial");
-//     //console.log(pagesSelected.style.f)
+//CHANGING TEXT WHEN BUTTON IS SELECTED
+
+const timesNewRomanBtn = document.getElementById("js-Times-New-Roman-btn");
+const arialBtn = document.getElementById("js-Arial-btn");
+const pagesSelected = document.getElementById("base-page-1"); //could later change to all pages
+
+const boldContainer = document.getElementById("js-text-bold-container");
+const italicContainer = document.getElementById("js-italic");
+
+
+
+const toggleFunction = function(classL){
+    pagesSelected.classList.toggle(classL);
+}
+
+timesNewRomanBtn.addEventListener("click",function(e){
+    e.stopPropagation();
+    toggleFunction("add-Times-New-Roman");
+});
+
+arialBtn.addEventListener("click",function(e){
+    e.stopPropagation();
+    toggleFunction("add-Arial");
+});
+
+//ADDING BOLD EVENT LISTENER
+boldContainer.addEventListener("click",function(e){
+    e.stopPropagation();
+    toggleFunction("add-bold");
+});
+
+
+//ADDING ITALIC
+italicContainer.addEventListener("click",function(e){
+    e.stopPropagation();
+    toggleFunction("add-italic");
+});
+
+//highglighting
+// const highlightContainer = document.getElementById("js-text-highlight-container");
+// const selectedText = window.getSelection().toString();
+    
+// highlightContainer.addEventListener("click",()=>{
+//     if(selectedText.length >0){
+//            //wrapping selected text in span
+//            const span = document.createElement("span");
+//            span.className = "add-highlight";
+//            span.textContent = selectedText;
+//            console.log(selectedText);
+
+//            const text = pagesSelected.textContent;
+//            const replace = text.replace(selectedText,span.outerHTML);
+//            pagesSelected.innerHTML = replace; //replacing old with new
+//     }
+            
 // });
